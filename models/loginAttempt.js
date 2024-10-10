d08a26e1019829e6998cd9cb5755ac166c44cf1d
Emmett922 <emmett.peters922@gmail.com>
2024-10-10 17:10:39 -0400

    Fixed the logic for the sequencing of user login attempts.

--- models/loginAttempt.js
+++ models/loginAttempt.js
@@ -1,31 +1,45 @@
-const mongoose = require('mongoose')
-const AutoIncrement = require('mongoose-sequence')(mongoose) // Import mongoose-sequence plugin
+const mongoose = require("mongoose");
 
-const loginAttemptSchema = new mongoose.Schema({
+const loginAttemptSchema = new mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     // Auto-incr. attempt num. for tracking login attmepts
     attemptNum: {
-        type: Number
+      type: Number,
     },
     // Indicate attempt success
     successful: {
-        type: Boolean,
-        default: false
-    }
-}, {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
     timestamps: {
-        createdAt: true,    // Gives the date and time when attempt was made
-        updatedAt: false    // Don't need this
-    }
-})
+      createdAt: true, // Gives the date and time when attempt was made
+      updatedAt: false, // Don't need this
+    },
+  }
+);
 
-// Apply mongoose-sequence plugin
-loginAttemptSchema.plugin(AutoIncrement, {
-    inc_field: 'attemptNum'
-})
+// Pre-save hook to auto-increment attemptNum per user
+loginAttemptSchema.pre("save", async function (next) {
+  const loginAttempt = this;
 
-module.exports = mongoose.model('LoginAttempt', loginAttemptSchema)
+  // FInd the latest attempt number for the user
+  const latestAttempt = await mongoose
+    .model("LoginAttempt")
+    .findOne({ user: loginAttempt.user })
+    .sort({ attempt: -1 })
+    .exec();
+
+  // Set attemptNum to 1 if no previous attempts, otherwise increment
+  loginAttempt.attemptNum = latestAttempt ? latestAttempt.attemptNum + 1 : 1;
+
+  next();
+});
+
+module.exports = mongoose.model("LoginAttempt", loginAttemptSchema);
