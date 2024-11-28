38d5c87c1b60ba7549b4899c64e5c0cbee2f303f
Emmett922 <emmett.peters922@gmail.com>
2024-11-27 19:50:18 -0500

    Added new allowed origin for the render.com deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://www.ledgerlifeline.com',
-    'https://ledgerlifeline.com',
+    'https://ledgerlifeline.onrender.com',
     'http://localhost:3500'
 ]
 
